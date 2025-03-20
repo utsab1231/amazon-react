4f1e5c225b725cdb85687921a56027f088e9f01b
Utsab Adhikari <uadhikari@ecc.net>
2025-03-20 23:26:19 +0545

    #dummy testing commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
 import { useState } from 'react'
+import "./App.css";
 import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import viteLogo from "/vite.svg";
 
 function App() {
   const [count, setCount] = useState(0)
@@ -28,8 +28,9 @@ function App() {
       <p className="read-the-docs">
         Click on the Vite and React logos to learn more
       </p>
+      <h1>This is for testing value in the amazon aws web service.</h1>
     </>
-  )
+  );
 }
 
 export default App
